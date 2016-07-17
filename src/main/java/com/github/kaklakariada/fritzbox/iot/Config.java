d80086f52d06950766758365176cdd7ed16dcb96
kaklakariada <kaklakariada@users.noreply.github.com>
2016-07-17 11:06:39 +0200

    Create config class

---
+++ src/main/java/com/github/kaklakariada/fritzbox/iot/Config.java
@@ -0,0 +1,88 @@
+package com.github.kaklakariada.fritzbox.iot;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.kaklakariada.fritzbox.iot.util.KeyStorePasswordPair;
+import com.github.kaklakariada.fritzbox.iot.util.Util;
+
+public class Config {
+	private static final String CONFIG_FILE = "config/config.properties";
+
+	private final static Logger LOG = LoggerFactory.getLogger(Config.class);
+
+	private final Properties config;
+	private static Config instance;
+
+	private Config(Properties config) {
+		this.config = config;
+	}
+
+	public static Config get() {
+		if (instance == null) {
+			final Properties properties = readConfigFile(CONFIG_FILE);
+			instance = new Config(properties);
+		}
+		return instance;
+	}
+
+	private static Properties readConfigFile(String fileName) {
+		final Path path = Paths.get(fileName).toAbsolutePath();
+		LOG.info("Reading test config from {}", path);
+		final Properties config = new Properties();
+		try (InputStream stream = Files.newInputStream(path)) {
+			config.load(stream);
+		} catch (final IOException e) {
+			throw new RuntimeException("Could not load properties " + path, e);
+		}
+		return config;
+	}
+
+	public String getClientEndpoint() {
+		return getProperty("aws.iot.client_endpoint");
+	}
+
+	public String getClientId() {
+		return getProperty("aws.iot.client_id");
+	}
+
+	public String getThingName() {
+		return getProperty("aws.iot.thing_name");
+	}
+
+	private Path getCertificateFile() {
+		return getPathProperty("aws.iot.certificate_file");
+	}
+
+	private Path getPrivateKeyFile() {
+		return getPathProperty("aws.iot.privateKey_file");
+	}
+
+	public KeyStorePasswordPair getKeyStorePasswordPair() {
+		return Util.getKeyStorePasswordPair(getCertificateFile(), getPrivateKeyFile());
+	}
+
+	private String getProperty(String propertyKey) {
+		final String value = config.getProperty(propertyKey);
+		if (value != null) {
+			return value;
+		}
+		throw new AssertionError("Property '" + propertyKey + "' not found in " + CONFIG_FILE);
+	}
+
+	private Path getPathProperty(String propertyKey) {
+		final Path path = Paths.get(getProperty(propertyKey)).toAbsolutePath();
+		if (!Files.isReadable(path)) {
+			throw new RuntimeException("Error reading file " + path);
+		}
+		return path;
+	}
+
+}
